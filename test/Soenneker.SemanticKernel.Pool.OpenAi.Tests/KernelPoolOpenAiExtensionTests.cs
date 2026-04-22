4e4beaba8aafd8fa01abbb9b8dba0e6b7d9671ff
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:38 -0500

    TUnit conversion

--- test/Soenneker.SemanticKernel.Pool.OpenAi.Tests/KernelPoolOpenAiExtensionTests.cs
+++ test/Soenneker.SemanticKernel.Pool.OpenAi.Tests/KernelPoolOpenAiExtensionTests.cs
@@ -1,16 +1,15 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.SemanticKernel.Pool.OpenAi.Tests;
 
-[Collection("Collection")]
-public class KernelPoolOpenAiExtensionTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class KernelPoolOpenAiExtensionTests : HostedUnitTest
 {
-    public KernelPoolOpenAiExtensionTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public KernelPoolOpenAiExtensionTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
